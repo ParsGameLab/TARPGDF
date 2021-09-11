3d8537951fbb0d19fc534fcd8f279284341f858f
YuChTai <rege2011402@gmail.com>
2021-09-11 17:10:29 +0800

    補傳 9/8/2021 的內容 程式：十字準線偵測（攝影機中點，單一直線射線檢測）。（敵人角色的血量條，將只在被主攝影機中點對準的時候才會顯示。） 抬頭顯示：敵人角色的血量條之Scale比例，全部統一為(0.03, 0.02, 0)，並製作成預製體。 程式：修 模型：敵人角色的Scale的比例，全部統一為(0.55, 0.55, 0.55)，並製作成預製體。 程式：新增「待機回血」。（敵人角色處於木人狀態時，短暫時間內未受到攻擊，將主動回充血量。（原先為血量值小於0時，才會觸發（保留））

--- Assets/_2_Auxiliary Work/_Scripts/Enemy/Class/IEnemy_Base.cs
+++ Assets/_2_Auxiliary Work/_Scripts/Enemy/Class/IEnemy_Base.cs
@@ -5,16 +5,17 @@ namespace iii_UMVR06_TPSDefenseGame_Subroutines_2 {
     public abstract class IEnemy_Base : MonoBehaviour {
 
         #region Field
-        protected HealthSystem reference_HealthSystem;
+        protected Camera mainCamera;
         protected HealthBar myHealthBar;
+        protected HealthSystem reference_HealthSystem;
 
         protected float currentHealthPoint;
-        [SerializeField] protected float maxHealthPoint;
+        [SerializeField] 
+        protected float maxHealthPoint;
 
-        public bool isWoodendummy = true;
-        private bool isInvincible = false;
-
-        private float delayTimer = 0;
+        //float middlePointX_mainCameraEyes;
+        //float middlePointY_mainCameraEyes;
+        //Vector3 middlePoint_mainCameraEyes;
         #endregion
 
         #region Function
@@ -23,58 +24,133 @@ namespace iii_UMVR06_TPSDefenseGame_Subroutines_2 {
         }
 
         protected virtual void Start() {
+            mainCamera = Camera.main;
+            myHealthBar.SetUp_MainCamera(mainCamera);
+            //middlePointX_mainCameraEyes = mainCamera.pixelWidth / 2;
+            //middlePointY_mainCameraEyes = mainCamera.pixelHeight / 2;
+            //middlePoint_mainCameraEyes = new Vector3(middlePointX_mainCameraEyes, middlePointY_mainCameraEyes, 0);
+
             reference_HealthSystem = new HealthSystem(maxHealthPoint);
 
             currentHealthPoint = maxHealthPoint;
 
             myHealthBar.SetUp_HealthSystem(reference_HealthSystem);
-            myHealthBar.SetSize();
-            myHealthBar.Hide();
-            reference_HealthSystem.OnHealthCharging += Reference_HealthSystem_OnHealthCharging;
+            myHealthBar.SetSize();            
         }
 
-        protected virtual void Update() {
-            if(isInvincible) {
-                delayTimer = 0;
-                Invoke("BeginCharge", .75f);
-            } else {
-                delayTimer += Time.deltaTime;
+        public virtual void Update() {
+            
+            HasBeAimedAlready();
+
+            /*******************************************
+             * 僅在 Project Review 時使用。
+             ******************************************/
+            if(isAWoodendummy && eventCount == 0) {
+                reference_HealthSystem.OnHealthCharging += Reference_HealthSystem_OnHealthCharging;
+                eventCount++;
+            } else if(!isAWoodendummy && eventCount > 0) {
+                reference_HealthSystem.OnHealthCharging -= Reference_HealthSystem_OnHealthCharging;
+                eventCount--;
             }
+
+            OnProjectReview();
         }
         #endregion
 
         #region Method
+        //public virtual void UnderAttack(float damagePoint) {
+        //    currentHealthPoint -= (int)damagePoint;
+        //    reference_HealthSystem.Calculate_HealthPoint_Damage(damagePoint);
+        //}
+
+        private void HasBeAimedAlready() {       
+            Ray middlePointRay_mainCamera = mainCamera.ScreenPointToRay(CalculateTheCrossHairPosition());
+            RaycastHit hitInfo;
+            if(Physics.Raycast(middlePointRay_mainCamera, out hitInfo, 750f)) {
+                if(hitInfo.transform.GetComponent<Collider>().CompareTag("Enemy")) {
+                    GameObject beHitterHealthBar = hitInfo.transform.GetChild(0).transform.Find("HealthBar").gameObject;
+                    beHitterHealthBar.SetActive(true);
+                } else {
+                    myHealthBar.gameObject.SetActive(false);
+                }
+            } else {
+                myHealthBar.gameObject.SetActive(false);
+            }
+        }
+
+        private Vector3 CalculateTheCrossHairPosition() {
+            float middlePointX_mainCameraEyes = mainCamera.pixelWidth / 2;
+            float middlePointY_mainCameraEyes = mainCamera.pixelHeight / 2;
+            Vector3 middlePoint_mainCameraEyes = new Vector3(middlePointX_mainCameraEyes, middlePointY_mainCameraEyes, 0);
+            return middlePoint_mainCameraEyes;
+        }
+        #endregion
+
+        /*******************************************
+         * 僅在 Project Review 時使用。
+         ******************************************/
+        #region Field
+        public bool isAWoodendummy = true;
+        private int eventCount = 0;
+        private bool isInvincible = false;
+
+        private float delayTimer = 0;
+        #endregion
+
+        #region Method
+        private void OnProjectReview() {
+            if(isInvincible) {
+                delayTimer = 0;
+                Invoke("BeginCharge", .15f);
+            } else {
+                delayTimer += Time.deltaTime;
+                if(isAWoodendummy && delayTimer > .85f) {
+                    Invoke("BeginRestore", .35f);
+                }
+            }
+        }
         public virtual void UnderAttack(float damagePoint) {
-            if(isInvincible || delayTimer < 1f) {
+            if(isInvincible) {
                 return;
             }
 
-            if(myHealthBar.BarSpriteColor != Color.red) {
-                myHealthBar.SetColor(Color.red);
-            }
+            delayTimer = 0;
+            myHealthBar.SetColor(Color.red);
 
             currentHealthPoint -= (int)damagePoint;
-            reference_HealthSystem.Calculate_HealthPoint_Damage(damagePoint, isWoodendummy);
-            myHealthBar.Show();
+            reference_HealthSystem.Calculate_HealthPoint_Damage(damagePoint, isAWoodendummy);
+        }
+        private void BeginCharge() {
+            Charge_Broken(Time.deltaTime * 265f);            
         }
 
-        protected virtual void ChargeHealth(float chargePoint) {
+        private void BeginRestore() {
+            Restore_Standby(Time.deltaTime * 235f);
+        }
+
+        protected virtual void Charge_Broken(float chargePoint) {
             currentHealthPoint += (int)chargePoint;
             if(currentHealthPoint % 1f == 0) {
                 myHealthBar.SetColor(Color.green);
             }
 
-            reference_HealthSystem.Calculate_HealthPoint_Charge(chargePoint);
+            reference_HealthSystem.Calculate_HealthPoint_Heal(chargePoint);
 
             if(currentHealthPoint >= maxHealthPoint) {
                 currentHealthPoint = maxHealthPoint;
-                myHealthBar.Hide();
+                myHealthBar.SetColor(Color.red);
                 isInvincible = false;
             }
         }
 
-        private void BeginCharge() {
-            ChargeHealth(Time.deltaTime * 175f);            
+        protected virtual void Restore_Standby(float restorPoint) {
+            currentHealthPoint += (int)restorPoint;
+
+            reference_HealthSystem.Calculate_HealthPoint_Heal(restorPoint);
+
+            if(currentHealthPoint >= maxHealthPoint) {
+                currentHealthPoint = maxHealthPoint;
+            }
         }
         #endregion
 
--- Assets/_2_Auxiliary Work/_Scripts/Enemy/System/HealthSystem.cs
+++ Assets/_2_Auxiliary Work/_Scripts/Enemy/System/HealthSystem.cs
@@ -5,14 +5,13 @@ namespace iii_UMVR06_TPSDefenseGame_Subroutines_2 {
 
     public class HealthSystem {
 
-        #region Field
-        public event EventHandler OnHealthChanged;
-        public event EventHandler OnHealthCharging;
+        #region Event
+        public event EventHandler OnHealthChanged;    
+        #endregion
 
+        #region Field
         private int currentHealthPoint;
         private int maxHealthPoint;
-
-        private bool isWoodendummy = false;
         #endregion
 
         #region Constructor
@@ -33,17 +32,15 @@ namespace iii_UMVR06_TPSDefenseGame_Subroutines_2 {
             return currentHealthPoint / (float)maxHealthPoint;
         }
 
-        public void Calculate_HealthPoint_Damage(float theDamagePoint, bool checkIsWoodendummy) {
-            isWoodendummy = checkIsWoodendummy;
+        public void Calculate_HealthPoint_Damage(float theDamagePoint) {
             currentHealthPoint -= (int)theDamagePoint;
             if(currentHealthPoint <= 0) { 
                 currentHealthPoint = 0;
-                HasBeganCharging();
             }
             OnHealthChanged?.Invoke(this, EventArgs.Empty);
         }
 
-        public void Calculate_HealthPoint_Charge(float theChargePoint) {
+        public void Calculate_HealthPoint_Heal(float theChargePoint) {
             currentHealthPoint += (int)theChargePoint;
             if(currentHealthPoint >= maxHealthPoint) {
                 currentHealthPoint = maxHealthPoint;
@@ -52,13 +49,37 @@ namespace iii_UMVR06_TPSDefenseGame_Subroutines_2 {
 
         }
 
+
+        #endregion
+
+        /*******************************************
+         * 僅在 Project Review 時使用。
+         ******************************************/
+        #region Event
+        public event EventHandler OnHealthCharging;
+        #endregion
+
+        #region Field
+        private bool isAWoodendummy = false;
+        #endregion
+
+        #region Method
+        public void Calculate_HealthPoint_Damage(float theDamagePoint, bool checkIsAWoodendummy) {
+            isAWoodendummy = checkIsAWoodendummy;
+            currentHealthPoint -= (int)theDamagePoint;
+            if(currentHealthPoint <= 0) {
+                currentHealthPoint = 0;
+                HasBeganCharging();
+            }
+            OnHealthChanged?.Invoke(this, EventArgs.Empty);
+        }
+
         public void HasBeganCharging() {
-            if(currentHealthPoint <= 0 && isWoodendummy) {
+            if(currentHealthPoint <= 0 && isAWoodendummy) {
                 OnHealthCharging?.Invoke(this, EventArgs.Empty);
             }
         }
         #endregion
-
     }
 
 }
