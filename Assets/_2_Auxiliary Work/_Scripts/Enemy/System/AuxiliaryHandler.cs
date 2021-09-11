970fdc6ee6e889b2226dfa84878167eb9377dbe9
ElioElderAsia <ElioElder@outlook.com>
2021-09-11 16:17:04 +0800

    補傳 9/8/2021 的內容

--- Assets/_2_Auxiliary Work/_Scripts/Enemy/System/AuxiliaryHandler.cs
+++ Assets/_2_Auxiliary Work/_Scripts/Enemy/System/AuxiliaryHandler.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace iii_UMVR06_TPSDefenseGame_Subroutines_2 {
 
-    public class Slime_Red : IEnemy_Base {
+    public class AuxiliaryHandler : MonoBehaviour {
 
     }
 
